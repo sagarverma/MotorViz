f9a5840aab039a8594fe39afa161b7ad58a003b7
Sagar <sagar15056@iiitd.ac.in>
2020-03-09 12:19:10 +0100

    Learned about props and state.

--- motorviz/src/App.js
+++ motorviz/src/App.js
@@ -4,10 +4,31 @@ import ReactDOM from 'react-dom';
 class Car extends React.Component {
   constructor() {
     super();
-    this.state = {color: "red"};
+    this.state = {
+      color: "black",
+      brand: 'maruti',
+      model: 'alto 800',
+      year: '2014'
+    };
+  }
+  changeColor = () => {
+    this.setState({color: "blue"});
   }
   render () {
-    return <h1>Hi, I am a {this.props.color} Car!</h1>;
+    return (
+      <div>
+        <h1>Hi, I am a {this.state.brand} Car!</h1>
+        <p>
+          It is a {this.state.color} &nbsp;
+          {this.state.model} &nbsp;
+          from {this.state.year}.
+        </p>
+        <button
+          type="button"
+          onClick={this.changeColor}
+        >Change color</button>
+      </div>
+    );
   }
 }
 
