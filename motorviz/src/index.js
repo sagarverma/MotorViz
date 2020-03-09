6343942ae013c9294a9b048e1de451c34a4b0ec8
Sagar <sagar15056@iiitd.ac.in>
2020-03-09 22:29:43 +0100

    victory js play

--- motorviz/src/index.js
+++ motorviz/src/index.js
@@ -1,67 +1,11 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
+import React, {Component} from "react";
+import {render} from "react-dom";
+import {VictoryPie} from "victory";
 
-
-class MyForm extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      description: 'The content of a textarea goes in the value attribute',
-      username: '',
-      age: null,
-      errormessage: ''
-    };
-  }
-  mySubmitHandler = (event) => {
-    event.preventDefault();
-    let age = this.state.age;
-      if (!Number(age)){
-        alert("Your age must be a number");
-      }
-
-    alert("You are submitting " + this.state.username);
-  }
-  myChangeHandler = (event) => {
-    let nam = event.target.name;
-    let val = event.target.value;
-    let err = '';
-    if (nam === "age"){
-      if (val != "" && !Number(val)) {
-        err = <strong>Your age must be a number</strong>;
-      }
-    }
-    this.setState({errormessage: err});
-    this.setState({[nam]: val});
-  }
+class PieChart extends Component {
   render() {
-    const mystyle = {
-      color: "Blue",
-      backgroundcolor: "DodgerBlue",
-      padding: "10px",
-      fontFamily: "Arial"
-    };
-    return (
-        <form onSubmit={this.mySubmitHandler} style={mystyle}>
-        <h1>Hello {this.state.username} {this.state.age}</h1>
-        <textarea value={this.state.description} />
-        <p>Enter your name:</p>
-        <input
-          type='text'
-          name='username'
-          onChange={this.myChangeHandler}
-        />
-        <p>Enter you age:</p>
-        <input
-          type='text'
-          name='age'
-          onChange={this.myChangeHandler}
-        />
-        {this.state.errormessage}
-
-        <input type='submit' />
-        </form>
-    );
+    return <VictoryPie />;
   }
 }
 
-ReactDOM.render(<MyForm />, document.getElementById('root'));
+render(<PieChart />, document.getElementById("root"));
