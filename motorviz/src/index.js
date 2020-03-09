368c2b949e0b7681ae4147de21d0302d183164ea
Sagar <sagar15056@iiitd.ac.in>
2020-03-09 18:03:20 +0100

    Doing forms.

--- motorviz/src/index.js
+++ motorviz/src/index.js
@@ -2,15 +2,58 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 
 
-class Football extends React.Component {
-  shoot = (a, b) => {
-    alert(b.type);
+class MyForm extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      username: '',
+      age: null,
+      errormessage: ''
+    };
+  }
+  mySubmitHandler = (event) => {
+    event.preventDefault();
+    let age = this.state.age;
+      if (!Number(age)){
+        alert("Your age must be a number");
+      }
+
+    alert("You are submitting " + this.state.username);
+  }
+  myChangeHandler = (event) => {
+    let nam = event.target.name;
+    let val = event.target.value;
+    let err = '';
+    if (nam === "age"){
+      if (val != "" && !Number(val)) {
+        err = <strong>Your age must be a number</strong>;
+      }
+    }
+    this.setState({errormessage: err});
+    this.setState({[nam]: val});
   }
   render() {
     return (
-      <button onClick={(ev) => this.shoot("Goal", ev)}>Take the shot!</button>
+        <form onSubmit={this.mySubmitHandler}>
+        <h1>Hello {this.state.username} {this.state.age}</h1>
+        <p>Enter your name:</p>
+        <input
+          type='text'
+          name='username'
+          onChange={this.myChangeHandler}
+        />
+        <p>Enter you age:</p>
+        <input
+          type='text'
+          name='age'
+          onChange={this.myChangeHandler}
+        />
+        {this.state.errormessage}
+        
+        <input type='submit' />
+        </form>
     );
   }
 }
 
-ReactDOM.render(<Football />, document.getElementById('root'));
+ReactDOM.render(<MyForm />, document.getElementById('root'));
