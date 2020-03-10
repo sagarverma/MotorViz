ae50530d94c9f8bc685452450935bf959b4e363c
Sagar Verma <sagar15056@iiitd.ac.in>
2020-03-10 23:44:49 +0100

    Able to plot a graph. No interactions.

--- motorviz/src/index.js
+++ motorviz/src/index.js
@@ -1,11 +1,43 @@
-import React, {Component} from "react";
-import {render} from "react-dom";
-import {VictoryPie} from "victory";
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { VictoryLine, VictoryChart, VictoryAxis, VictoryTheme } from 'victory';
 
-class PieChart extends Component {
+const data = [
+  {x: 0, y: 0},
+  {x: 1, y: 0},
+  {x: 1.5, y: 50},
+  {x: 3, y: 50}
+];
+
+class App extends React.Component {
   render() {
-    return <VictoryPie />;
+    return (
+      <VictoryChart
+      theme={VictoryTheme.material}
+      domainPadding={20}
+      >
+      <VictoryAxis crossAxis
+        domain={[0, 4]}
+        theme={VictoryTheme.material}
+        label="Speed (Hz)"
+        style={{axisLabel: {fontSize: 10, padding: 30}}}
+      />
+      <VictoryAxis dependentAxis crossAxis
+        domain={[0, 60]}
+        theme={VictoryTheme.material}
+        label="Time (s)"
+        style={{axisLabel: {fontSize: 10, padding: 30}}}
+      />
+        <VictoryLine
+          data={data}
+          style={{
+             data: { stroke: "#c43a31" },
+             parent: { border: "1px solid #ccc"}
+          }}
+        />
+      </VictoryChart>
+    )
   }
 }
 
-render(<PieChart />, document.getElementById("root"));
+ReactDOM.render(<App />, document.getElementById("root"));
