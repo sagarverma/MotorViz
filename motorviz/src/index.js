3aff0d472840ab26c7824b8ffefa21d97cebea67
Sagar Verma <sagar15056@iiitd.ac.in>
2020-03-11 00:08:13 +0100

    inheritence implementation not working

--- motorviz/src/index.js
+++ motorviz/src/index.js
@@ -2,6 +2,44 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { VictoryLine, VictoryChart, VictoryAxis, VictoryTheme } from 'victory';
 
+
+class Quantity extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            label_x: "",
+            label_y: "",
+            domain_x: [],
+            domain_y: [],
+            data: {}
+        }
+    }
+    render() {
+        return (
+            <VictoryChart
+                theme={VictoryTheme.material}
+                domainPadding={20}
+            >
+            <VictoryAxis crossAxis
+                domain={this.state.domain_x}
+                theme={VictoryTheme.material}
+                label={this.state.label_x}
+                style={{axisLabel: {fontSize: 10, padding: 30}}}
+            />
+            <VictoryAxis dependentAxis crossAxis
+                domain={this.state.domain_y}
+                theme={VictoryTheme.material}
+                label={this.state.label_y}
+                style={{axisLabel: {fontSize: 10, padding: 30}}}
+            />
+            <VictoryLine
+                data={this.state.data}
+            />
+            </VictoryChart>
+        )
+    }
+}
+
 const data = [
   {x: 0, y: 0},
   {x: 1, y: 0},
@@ -12,30 +50,12 @@ const data = [
 class App extends React.Component {
   render() {
     return (
-      <VictoryChart
-      theme={VictoryTheme.material}
-      domainPadding={20}
-      >
-      <VictoryAxis crossAxis
-        domain={[0, 4]}
-        theme={VictoryTheme.material}
-        label="Speed (Hz)"
-        style={{axisLabel: {fontSize: 10, padding: 30}}}
-      />
-      <VictoryAxis dependentAxis crossAxis
-        domain={[0, 60]}
-        theme={VictoryTheme.material}
-        label="Time (s)"
-        style={{axisLabel: {fontSize: 10, padding: 30}}}
-      />
-        <VictoryLine
-          data={data}
-          style={{
-             data: { stroke: "#c43a31" },
-             parent: { border: "1px solid #ccc"}
-          }}
-        />
-      </VictoryChart>
+      <table>
+        <tr>
+            <td><Quantity label_x="Speed (Hz)" label_y="Time (s)" domain_x={[0, 4]} domain_y={[0, 50]} data={data}/></td>
+            <td> </td>
+        </tr>
+      </table>
     )
   }
 }
